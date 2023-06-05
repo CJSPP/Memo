d414502887b3cd878720827109a0f793407e86b7
Eunbee Lee <owiiwo928@gmail.com>
2023-06-05 09:56:14 +0900

    Memo 뼈대

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("1. 입력");
+        System.out.println("2. 목록 보기");
+        System.out.println("3. 수정");
+        System.out.println("4. 삭제");
+        System.out.println("5. 종료");
+
+        int num = sc.nextInt();
+        switch (num) {
+            case 1 : {
+                System.out.println("");
+            }
+            case 2 : {
+                System.out.println("");
+            }
+            case 3 : {
+                System.out.println("");
+            }
+            case 4 : {
+                System.out.println("");
+            }
+            case 5 : {
+                System.out.println("");
+            }
+        }
+    }
+}
---
+++ src/Memo.java
@@ -0,0 +1,27 @@
+public class Memo {
+    private int number;
+    private String name;
+    private String password;
+    private String memo;
+    private String date;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public String getDate() {
+        return date;
+    }
+}
