b1345b6ed6b08e44e910c4ae6639421676e4d28c
wlstj <redvoca100466@gmail.com>
2023-06-05 13:07:34 +0900

    입력 기능 추가했습니다!!!

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         while (true) {
             System.out.println("1. 입력");
             System.out.println("2. 목록 보기");
@@ -19,22 +19,37 @@ public class Main {
             if (num == 5)
                 break;
             switch (num) {
-                case 1 :
+                case 1:
                     //입력 메서드를 만들어서 호출하면 될거 같습니다.
+                    memo_input();
                     break;
-                case 2 :
+                case 2:
                     printMemolist();
                     break;
-                case 3 :
+                case 3:
                     //수정 메서드를 만들어서 호출하면 될거 같습니다.
                     break;
-                case 4 :
+                case 4:
                     deleteMemo();
                     break;
             }
         }
     }
-    
+
+    private static void memo_input() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("이름을 입력해주세요.");
+        String name = sc.nextLine();
+        System.out.println("비밀번호를 입력해주세요.");
+        String password = sc.nextLine();
+        System.out.println("메모를 입력해주세요.");
+        String memo = sc.nextLine();
+
+        Memo memoclass = new Memo(name, password, memo);
+        MemoList.memolist.add(memoclass);
+    }
+
     private static void printMemolist() {
         System.out.println("[\u001B[34m MemoList \u001B[0m]");
         List<Memo> list = memolist.getMemolist();
@@ -44,9 +59,9 @@ public class Main {
         System.out.println("\n");
     }
 
-  
+
     private static void deleteMemo() { // 삭제 기능
-        Scanner sc  = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("삭제할 글 번호를 입력해주세요.");
         System.out.print("\n여기에 글 번호를 입력해주세요 => ");
         int deleteNumber = sc.nextInt();
@@ -57,13 +72,13 @@ public class Main {
                 System.out.println("해당 글의 비밀번호를 입력하시면 바로 메모가 삭제됩니다.");
                 System.out.print("해당 글의 비밀번호를 입력해주세요. =>");
                 String pwd = sc.nextLine();
-                if(pwd.equals(memolist.getMemolist().get(i).getPassword())) {
+                if (pwd.equals(memolist.getMemolist().get(i).getPassword())) {
                     memolist.getMemolist().remove(i);
                     break;
                 }
             }
         }
-        if(!exist) {
+        if (!exist) {
             System.out.println("삭제할 글이 존재하지 않습니다.");
         }
     }
